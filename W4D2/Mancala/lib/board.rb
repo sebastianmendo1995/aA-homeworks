792c886eee476ba63bfd39075ca2d14ac58b8d7e
Sebastian Mendo Lopez <mendo@Sebastians-MacBook-Pro.local>
2019-11-04 21:25:05 -0800

    Fix make_move and next_turn bugs

--- W4D2/Mancala/lib/board.rb
+++ W4D2/Mancala/lib/board.rb
@@ -42,25 +42,27 @@ class Board
       if pos != opponent_cup
         cups[pos] << :stone
         num_stones-=1 
+        break if num_stones == 0
       end
       pos += 1
       pos = 0 if pos == 14
     end
-    #current_player_name == name1 ? player_cup = 6 : player_cup = 13
-
+    
+    self.render
     return next_turn(pos)
     
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+
     #first i have to check if it end on current player cup
     if ending_cup_idx == 6 || ending_cup_idx == 13
       return :prompt
-    elsif cups[ending_cup_idx].empty? 
+    elsif cups[ending_cup_idx].empty?
       return :switch
     else
-      return cups[ending_cup_idx]
+      return ending_cup_idx
     end
   end
 
