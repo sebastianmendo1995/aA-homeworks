a392a37769d469d627cd858c89a330ebad94296b
Sebastian Mendo Lopez <mendo@Sebastians-MacBook-Pro.local>
2019-11-24 22:59:38 -0800

    Username and password basic validation

---
+++ W7D1/app_name/app/models/user.rb
@@ -0,0 +1,38 @@
+class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true
+    validates :password_digest, presence: {message: "Password can't be blank"}
+    validates :session_token, presence: true, uniqueness: true
+    validates :password, length: { minimum: 6, allow_nil: true}
+    before_validation :ensure_session_token
+
+    attr_reader :password
+
+    # This method makes sure that the user has a session_token set, setting one if none exists
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
+
+    #This is a helper method I like to write that uses 
+    # SecureRandom::urlsafe_base64 and returns a 16-digit pseudorandom string
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64(16)
+    end
+
+    # This method takes in a username and a password and returns the user that matches
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+    end
+    
+    #This method resets the user's session_token and saves the user
+    def reset_session_token!
+        self.session_token = User.generate_session_token
+        self.save
+        self.session_token
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+end
